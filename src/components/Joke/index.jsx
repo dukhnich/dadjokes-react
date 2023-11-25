9a3f530934588664387c7d2f463fa764b94dfd2f
Maryna Dukhnich <maryna.dukhnich@gmail.com>
2023-11-25 17:06:00 +0100

    add api and jokes list

--- src/components/Joke/index.jsx
+++ src/components/Joke/index.jsx
@@ -3,9 +3,9 @@ import { useState } from "react";
 import upLikeImg from "./img/like-up.png";
 import downLikeImg from "./img/like-down.png";
 
-const Joke = () => {
-  const [upLikes, setUpLikes] = useState(0);
-  const [downLikes, setDownLikes] = useState(0);
+const Joke = ({ avatar, name, text, likes, dislikes }) => {
+  const [upLikes, setUpLikes] = useState(likes);
+  const [downLikes, setDownLikes] = useState(dislikes);
   const changeLikes = (n) => {
     if (n > 0) {
       setUpLikes(upLikes + Number(n));
@@ -17,18 +17,11 @@ const Joke = () => {
     <div className="joke">
       <div className="joke__body">
         <div className="joke__user">
-          <img
-            className="user-avatar"
-            src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
-          />
-          <p className="user-name">Neroxx</p>
+          <img className="user-avatar" src={avatar} alt={name + " avatar"} />
+          <p className="user-name">{name}</p>
         </div>
 
-        <p className="joke__text">
-          The secret service isn't allowed to yell "Get down!" anymore when the
-          president is about to be attacked. Now they have to yell "Donald,
-          duck!"
-        </p>
+        <p className="joke__text">{text}</p>
       </div>
       <div className="joke__likes">
         <button
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,10 +1,22 @@
 import "./style.css";
 import Joke from "../../components/Joke";
+import { useState, useEffect } from "react";
 
 export const HomePage = () => {
+  const [jokes, setJokes] = useState(null);
+  useEffect(() => {
+    const getJokes = async () => {
+      const response = await fetch("http://localhost:4000/api/jokes");
+      const data = await response.json();
+      setJokes(data.result);
+    };
+    getJokes();
+  }, []);
   return (
     <div className="container">
-      <Joke />
+      {Array.isArray(jokes)
+        ? jokes.map((joke) => <Joke {...joke} key={joke.id} />)
+        : null}
     </div>
   );
 };
